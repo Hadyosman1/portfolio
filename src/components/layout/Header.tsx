9caadbc1f9c0cdace37979bf47c22e1e5834e02d
hady <osmanhady73@gmail.com>
2025-01-14 07:01:34 +0200

    Fix Header error and change scrollbar color

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,12 +4,15 @@ import { ThemeToggle } from "../Theme";
 import NavLinks from "./NavLinks";
 import HireMeButton from "../HireMeButton";
 import MobileMenu from "./MobileMenu";
+import { Suspense } from "react";
 
 const Header = () => {
   return (
     <header className="sticky top-0 z-10 h-[64px] bg-background/60 shadow shadow-border backdrop-blur-sm">
       <nav className="container flex items-center justify-between gap-3 py-3">
-        <MobileMenu className="inline-flex md:hidden" />
+        <Suspense>
+          <MobileMenu className="inline-flex md:hidden" />
+        </Suspense>
 
         <Link href="/">
           <Logo className="size-10" />
